b77ba4ed3eacadd8eec69f421be316a30877d07b
Ember <Ember@qq.com>
2026-05-29 12:06:35 +0800

    Improve player queue and PiP controls

--- Application/youtube_core/src/main/cpp/types/libyoutubecore/Index.d.ts
+++
@@ -1,4 +0,0 @@
-export const initCipherEnv: () => number;
-export const destroyCipherEnv: () => void;
-export const executeCipher: (jsCode: string, functionName: string, param: string) => string;
-export const batchExecuteCipher: (jsCode: string, callsJson: string) => string;
